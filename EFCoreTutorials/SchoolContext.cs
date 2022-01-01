23c1c86e45e764fde59858b93079e5548e2c0de6
doggersen <doggersen@gmail.com>
2022-01-01 10:24:28 +0100

    finished the tutorial. but not sure if it really works, or how to even test that it works....

--- EFCoreTutorials/SchoolContext.cs
+++ EFCoreTutorials/SchoolContext.cs
@@ -7,11 +7,14 @@ using System.Threading.Tasks;
 
 namespace EFCoreTutorials
 {
-    public class SchoolContext : DbContext
+    internal class SchoolContext : DbContext
     {
-        public DbSet<Student> Students { get; set; }
+        public DbSet<Student>? Students { get; set; }
         public DbSet<Course> Courses { get; set; }
 
-
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True;");
+        }
     }
 }
